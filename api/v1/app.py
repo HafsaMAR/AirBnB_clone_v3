cc038217a0a1da9dfbe3d5cead085a90d69eb207
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-07 23:46:02 +0100

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,5 +12,8 @@ app.register_blueprint(app_views)
 def close_storage(exception):
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'), port=int(os.getenv('HBNB_API_PORT', 5000)), threaded=True)
+    host = os.getenv("HBNB_API_HOST", '0.0.0.0')
+    port = int(os.getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
