870467819b778f5033d8c3a3ee1e882cb9474314
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-08 00:23:20 +0100

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """endpoint will be to return the status of your API"""
 from flask import Flask, Blueprint
 from models import storage
-
+import os
 from api.v1.views import app_views
 
 app = Flask(__name__)
@@ -12,18 +12,18 @@ app.register_blueprint(app_views)
 def close_storage(exception):
     storage.close()
 
-# if os.getenv("HBNB_API_HOST"):
-#     host = "HBNB_API_HOST"
-# else:
-#     host = '0.0.0.0'
+if os.getenv("HBNB_API_HOST"):
+    host = "HBNB_API_HOST"
+else:
+    host = '0.0.0.0'
 
-# if os.getenv("HBNB_API_PORT"):
-#     port = "HBNB_API_PORT"
-# else:
-#     port = 5000
+if os.getenv("HBNB_API_PORT"):
+    port = "HBNB_API_PORT"
+else:
+    port = 5000
 
 if __name__ == '__main__':
-    import os
-    host = os.getenv("HBNB_API_HOST", '0.0.0.0')
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+    
+    # host = os.getenv("HBNB_API_HOST", '0.0.0.0')
+    # port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
