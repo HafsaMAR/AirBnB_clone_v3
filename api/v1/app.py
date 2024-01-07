5d2e37162456d69d6a68c85fd798e25755301412
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-08 00:04:37 +0100

    update check

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,17 +12,17 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def close_storage(exception):
     storage.close()
 
-if os.getenv("HBNB_API_HOST"):
-    host = "HBNB_API_HOST"
-else:
-    host = '0.0.0.0'
+# if os.getenv("HBNB_API_HOST"):
+#     host = "HBNB_API_HOST"
+# else:
+#     host = '0.0.0.0'
 
-if os.getenv("HBNB_API_PORT"):
-    port = "HBNB_API_PORT"
-else:
-    port = 5000
+# if os.getenv("HBNB_API_PORT"):
+#     port = "HBNB_API_PORT"
+# else:
+#     port = 5000
 
 if __name__ == '__main__':
-    # host = os.getenv("HBNB_API_HOST", '0.0.0.0')
-    # port = int(os.getenv("HBNB_API_PORT", 5000))
+    host = os.getenv("HBNB_API_HOST", '0.0.0.0')
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,6 @@
 from flask import Flask, jsonify
 from api.v1.views import app_views
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False, methods=["GET"])
 def status():
     return jsonify({"status": 'OK'})
