685d0e8270deea6722104d4b23609c69204a41cb
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-08 00:21:33 +0100

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,11 @@
 """endpoint will be to return the status of your API"""
 from flask import Flask, Blueprint
 from models import storage
-import os
+
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close_storage(exception):
@@ -23,6 +23,7 @@ def close_storage(exception):
 #     port = 5000
 
 if __name__ == '__main__':
+    import os
     host = os.getenv("HBNB_API_HOST", '0.0.0.0')
     port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,4 +6,4 @@ from api.v1.views import app_views
 
 @app_views.route('/status', strict_slashes=False, methods=["GET"])
 def status():
-    return jsonify({"status": 'OK'})
+    return jsonify(status="OK")
