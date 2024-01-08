3dac6cbc7cd814003d9f182123362c935e19b5b6
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-08 02:37:17 +0100

    code update

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,11 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask('__name__')
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close_storage(exception):
+    """close storage in case of teardown"""
     storage.close()
 
 
