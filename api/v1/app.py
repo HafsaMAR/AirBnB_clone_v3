a47f65b5e61459f0677a2b257a8154d05e33cf85
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-10 02:28:01 +0100

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,9 +11,9 @@ import os
 from api.v1.views import app_views
 
 app = Flask('__name__')
-
-app.register_blueprint(app_views, url_prefix='/api/v1')
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
 
 
 @app.teardown_appcontext
