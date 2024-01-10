8191d228feeb1a63d1dc1d5847f45510d1a1108d
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-10 02:24:13 +0100

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,13 @@ endpoint will be to return the status of your API
 """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 import os
 from api.v1.views import app_views
 
 app = Flask('__name__')
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
